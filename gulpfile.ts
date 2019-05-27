efe9d8fffe9c5e701a9ecbc927641923db75c1fe
La source <la-source@hotmail.be>
2019-05-27 17:58:12 +0200

    remove useless files & dependencies

--- gulpfile.ts
+++
@@ -1,8 +0,0 @@
-import * as gulp from "gulp";
-import {createProject} from "gulp-typescript";
-
-gulp.task("default", () =>
-    gulp.src("src/**/*.ts")
-        .pipe(createProject("tsconfig.json")())
-        .pipe(gulp.dest("dist")),
-);
