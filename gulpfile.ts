859e6da4ef4e3010c452df227f7cfa64b66d1617
La source <la-source@hotmail.be>
2019-05-30 11:56:08 +0200

    build process

---
+++ gulpfile.ts
@@ -0,0 +1,15 @@
+import * as gulp from "gulp";
+import {init, write} from "gulp-sourcemaps";
+import {createProject} from "gulp-typescript";
+
+gulp.task("default", () =>
+    gulp.src("src/**/*.ts")
+        .pipe(init())
+        .pipe(createProject("tsconfig.json")())
+        .pipe(gulp.src([
+            "src/**/*.ejs",
+            "package*.json",
+        ]))
+        .pipe(write())
+        .pipe(gulp.dest("dist")),
+);
